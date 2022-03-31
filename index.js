33f094bcf170aeeb5c39c238db1171052966df2b
Chris Barber <chris@cb1inc.com>
2022-03-31 01:06:53 -0500

    Removed got and did things the hard way

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import got from 'got';
+import https from 'https';
 
 const url = 'https://raw.githubusercontent.com/tidev/organization-docs/main/AUTHORIZED_CONTRIBUTORS.md';
 
-const { body } = await got(url);
+const body = await new Promise((resolve, reject) => {
+	https.get(url, res => {
+		let buf = '';
+		res.on('data', data => {
+			buf += data.toString();
+		});
+		res.on('end', () => {
+			resolve(buf);
+		});
+		res.on('error', reject);
+	});
+});
 
 const signed = body
 	.match(/^\|.+\|$/mg)
 	?.slice(2)
 	.map(u => u.split('|')[2].trim());
 
-if (!signed.find(u => u === process.env.GITHUB_USER)) {
+if (signed.find(u => u === process.env.GITHUB_USER)) {
+	console.log('User is authorized');
+} else {
+	console.log('User not authorized');
 	process.exit(1);
 }
